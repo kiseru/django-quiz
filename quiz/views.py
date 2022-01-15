0048d35c441ae57bf0e3c8889fa4429095b1beb7
Alexandr.Kiselev <kis.alex97@gmail.com>
2022-01-15 17:50:41 +0300

    added showing questions for quiz

--- quiz/views.py
+++ quiz/views.py
@@ -23,4 +23,15 @@ class QuizDetailView(TemplateView):
         quizzes = get_quiz_data()
         filtered_quizzes = [quiz for quiz in quizzes if quiz.uuid == uuid]
         check_is_empty(quizzes)
-        return {"quiz": filtered_quizzes[0]}
+        return {
+            "quiz": filtered_quizzes[0],
+            "question": self.get_question(filtered_quizzes[0]),
+        }
+
+    def get_question(self, quiz):
+        self.check_has_question_id()
+        return quiz.questions[self.request.session["question_index"]]
+
+    def check_has_question_id(self):
+        if "question_index" not in self.request.session:
+            self.request.session["question_index"] = 0
